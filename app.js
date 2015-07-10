ad26913b1653b77ff21658b4b81828f12a1dd9a9
咸达 <xianda.xd@alibaba-inc.com>
2015-07-10 16:07:12 +0800

    update package.json

--- app.js
+++ app.js
@@ -23,5 +23,5 @@ app.use(router.routes()).use(router.allowedMethods());
 routes(router);
 
 app = module.exports = http.createServer(app.callback());
-app.listen(5000);
-console.log('listening on port 5000');
+app.listen(process.env.PORT || 3001);
+console.log('listening on port');
