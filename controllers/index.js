56cf7a9a1cf3a54b9f660ab8361c73f0c87efd27
咸达 <xianda.xd@alibaba-inc.com>
2015-07-11 18:57:12 +0800

    add simi

--- controllers/index.js
+++ controllers/index.js
@@ -15,28 +15,30 @@ exports.handle = function* () {
   console.log(content)
   var trigger_word = content.trigger_word;
   var text = content.text;
+  if (text.indexOf('!') === 0) {
+    return this.body = {};
+  }
   if (trigger_word === 'test') {
-    //yield utils.getImg('http://finance.sina.com.cn/stock/','test2', '#wmt_contents', 'iphone 5s');
-    //this.body = {
-    //  text: 'http://i.imgur.com/JQ8GA1p.jpg',
-    //  "attachments": [
-    //    {
-    //      "fallback": "Network traffic (kb/s): How does this look? @slack-ops - Sent by Julie Dodd - https://datadog.com/path/to/event",
-    //      "title": "Network traffic (kb/s)",
-    //      "title_link": "https://datadog.com/path/to/event",
-    //      "text": "How does this look? @slack-ops - Sent by Julie Dodd",
-    //      "image_url": "http://i.imgur.com/JQ8GA1p.jpg",
-    //      "color": "#764FA5"
-    //    }
-    //  ]
-    //};
+    yield utils.getImg('http://finance.sina.com.cn/stock/','test2', '#wmt_contents', 'iphone 5s');
+    this.body = {
+      text: 'http://i.imgur.com/JQ8GA1p.jpg',
+      "attachments": [
+        {
+          "fallback": "Network traffic (kb/s): How does this look? @slack-ops - Sent by Julie Dodd - https://datadog.com/path/to/event",
+          "title": "Network traffic (kb/s)",
+          "title_link": "https://datadog.com/path/to/event",
+          "text": "How does this look? @slack-ops - Sent by Julie Dodd",
+          "image_url": "http://i.imgur.com/JQ8GA1p.jpg",
+          "color": "#764FA5"
+        }
+      ]
+    };
   }
   if (text) {
-    var reply = yield simi.chat(text) || 呵呵;
+    var reply = yield simi.chat(text) || '呵呵';
     console.log(reply)
-    this.body = {
-      //text: '!' + reply
+    return this.body = {
+      text: '!' + reply
     };
-    this.status = 200;
   }
 };
